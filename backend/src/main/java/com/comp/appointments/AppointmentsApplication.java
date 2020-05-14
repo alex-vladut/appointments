a73560eee21faeb1d1579c80009de204a0bbe954
Alex Vladut <alexandruvladut@gmail.com>
2020-05-14 13:16:16 +0300

    fix(backend): enable cors headers

--- backend/src/main/java/com/comp/appointments/AppointmentsApplication.java
+++ backend/src/main/java/com/comp/appointments/AppointmentsApplication.java
@@ -2,14 +2,27 @@ package com.comp.appointments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 public class AppointmentsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppointmentsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentsApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
